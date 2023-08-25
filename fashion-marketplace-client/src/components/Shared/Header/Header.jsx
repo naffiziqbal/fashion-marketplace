6a2b0e287d8edc3164eb712d31ab1b156b3bfbf1
Nafiz Iqbal <f.nafiziqbal@gmail.com>
2023-08-25 19:03:07 +0600

    Responsive Nav

--- fashion-marketplace-client/src/components/Shared/Header/Header.jsx
+++ fashion-marketplace-client/src/components/Shared/Header/Header.jsx
@@ -3,80 +3,99 @@ import style from "./header.module.css";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import { Avatar } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import CloseSharpIcon from "@mui/icons-material/CloseSharp";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const handleMenuOpen = (isOpen) => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
   return (
-    <div className="flex justify-around  items-center  w-full pt-6 mb-10">
-      <div className="flex w-full justify-between">
-        <button className="font-bold text-xl">
+    <div className="flex justify-between w-full">
+      <div className="w-full">
+        <button className="font-bold text-3xl">
           <Link to={`/`}>Fashion House</Link>
         </button>
-        <div className=" relative top-3">
+      </div>
+      <div
+        className={`${
+          isOpen === true
+            ? "flex flex-col absolute h-full w-full top-24 bg duration-300 mr-0"
+            : "hidden md:flex flex-row  w-full justify-between"
+        } `}
+      >
+        <div className=" flex flex-row justify-between flex-nowrap ">
           <label htmlFor="searchField">
-            <SearchIcon className=" absolute top-3  left-4" />
             <input
               type="text"
               placeholder="Search Items"
-              className="lg:min-w-[358px] min-h-[48px] w-52  border rounded-3xl outline-none"
+              className="lg:min-w-[358px] min-h-[48px] w-full md:w-52  md:border border-b rounded-none md:rounded-3xl outline-none"
             />
           </label>
-        </div>
-      </div>
-      <nav className=" w-full lg:block md:block  hidden">
-        <ul className="absolute flex">
-          <li>
-            <Link>Home</Link>
-          </li>
-          <li>
-            <Link>Explore</Link>
-          </li>
-          <li>
-            <Link> Personal Collection</Link>
-          </li>
-          <li>
-            <Link>Drops</Link>
-          </li>
-          <li className={style.nested__nav}>
-            More
+          <nav className="">
             <ul className="">
               <li>
-                <Link>Stats</Link>
+                <Link>Home</Link>
               </li>
               <li>
-                <Link>Show</Link>
+                <Link>Explore</Link>
               </li>
               <li>
-                <Link>About Us</Link>
+                <Link>Personal Collection</Link>
+              </li>
+              <li>
+                <Link>Drops</Link>
+              </li>
+              <li className={style.nested__nav}>
+                More
+                <ul className={style.nested__nav_ul}>
+                  <li>
+                    <Link>Stats</Link>
+                  </li>
+                  <li>
+                    <Link>Show</Link>
+                  </li>
+                  <li>
+                    <Link>About Us</Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link>
+                  <NotificationsIcon />
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link>
+                  <ChatBubbleIcon />
+                </Link>
+              </li>
+              <li>
+                {" "}
+                <Link>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="/static/images/avatar/1.jpg"
+                    sx={{ width: 32, height: 32, background: "#744b32" }}
+                  />
+                </Link>
               </li>
             </ul>
-          </li>
-        </ul>
-      </nav>
-      <div className={style.user_nav_area}>
-        <ul className=" flex justify-between">
-          <li>
-            <Link>
-              <NotificationsIcon />
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link>
-              <ChatBubbleIcon />
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link>
-              <Avatar
-                alt="Remy Sharp"
-                src="/static/images/avatar/1.jpg"
-                sx={{ width: 32, height: 32, background: "#744b32" }}
-              />
-            </Link>
-          </li>
-        </ul>
+          </nav>
+        </div>
+      </div>
+      <div
+        className="z-50 block md:hidden"
+        onClick={() => handleMenuOpen(isOpen)}
+      >
+        {isOpen === true ? <CloseSharpIcon /> : <MenuIcon />}
       </div>
     </div>
   );
