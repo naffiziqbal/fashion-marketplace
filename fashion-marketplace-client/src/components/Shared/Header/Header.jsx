dadae9df946057653b015392cf0e554a2671a115
Nafiz Iqbal <f.nafiziqbal@gmail.com>
2023-09-03 21:20:57 +0600

    Modal

--- fashion-marketplace-client/src/components/Shared/Header/Header.jsx
+++ fashion-marketplace-client/src/components/Shared/Header/Header.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import style from "./header.module.css";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import { Avatar, Menu, MenuItem } from "@mui/material";
 import CloseSharpIcon from "@mui/icons-material/CloseSharp";
 import MenuIcon from "@mui/icons-material/Menu";
-import { useState } from "react";
+import {useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+  const location = useLocation()
+
 
 
   const handleMenuOpen = (isOpen) => {
@@ -18,20 +21,22 @@ const Header = () => {
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+  // if(location.pathname) return false
 
-  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+  if(location)
 
   return (
     <div className="flex w-full py-5">
-      <div className="w-full flex justify-between mx-6">
-        <button className="font-bold text-3xl">
-          <Link to={`/`}>Fashion House</Link>
+      <div className="w-full lg:w-auto  flex justify-between mx-6">
+        <button className="font-bold text-3xl text-primary">
+          <Link to={`/`}>FashionHouse</Link>
         </button>
         <div
           className="z-50 block lg:hidden"
@@ -46,7 +51,7 @@ const Header = () => {
           : "hidden lg:flex flex-row  w-full justify-between items-center"
           } `}
       >
-        <div className=" flex flex-col md: lg:flex-row gap-4 justify-between  mt-5">
+        <div className=" flex flex-col md: lg:flex-row gap-4 justify-between  mt-5 mx-16 lg:items-center">
           <label htmlFor="searchField">
             <input
               type="text"
